2475b715a9016d6d377b1c8a9346836c8d80d40f
GitAcrown <Acrown@outlook.fr>
2016-10-07 14:30:19 +0200

    Refonte du module Sticker

--- ProjectFreya/cogs/stick.py
+++ ProjectFreya/cogs/stick.py
@@ -7,19 +7,25 @@ from urllib import request
 import re
 import asyncio
 import os
+import random
 
 #Freya Exclusive
 
-class Stick:
-    """Ajout de stickers personnalisés."""
+default_img = {"IMG" : {}, "CAT" : {}}
+
+class Stock:
+    """Permet de stocker des données en local pour des fonctions utiles."""
 
     def __init__(self, bot):
         self.bot = bot
-        self.base = dataIO.load_json("data/stick/base.json")
+        self.img = dataIO.load_json("data/stock/img.json")
+        self.trg = dataIO.load_json("data/stock/trg.json")
 
-    @commands.group(pass_context=True, no_pm=True)
-    async def sticker(self, ctx):
-        """Gestion du module de Stickers."""
+    #------------ IMG --------------#
+
+    @commands.group(pass_context=True)
+    async def imgset(self, ctx):
+        """Gestion des images."""
         if ctx.invoked_subcommand is None:
             await send_cmd_help(ctx)
             msg = "```"
@@ -28,165 +34,191 @@ class Stick:
             msg += "```"
             await self.bot.say(msg)
 
-    @sticker.command(pass_context=True, no_pm=True)
+    @imgset.command(pass_context=True, no_pm=True)
     @checks.mod_or_permissions(kick_members=True)
-    async def add(self, ctx, nom, url):
-        """Ajoute un sticker à la base de données."""
-        if nom not in self.base:
-            filename = url.split('/')[-1]
-            if ".gif" in filename:
-                filename = filename[:-4]
-                filename = filename + ".png"
-                await self.bot.say("Fichier converti de '.gif' en '.png'")
-            try:
-                f = open(filename, 'wb')
-                f.write(request.urlopen(url).read())
-                f.close()
-                file = "data/autre/" + filename
-                os.rename(filename,file)
-                await self.bot.say("Fichier téléchargé et enregistré.")
-                self.base[nom] = {"CHEMIN": file, "URL": url}
-                fileIO("data/stick/base.json", "save", self.base)
-            except Exception as e:
-                print("Erreur, impossible de télécharger l'image : {}".format(e))
-                await self.bot.say("Désolé, mais cette image ne peut pas être téléchargée")
+    async def catadd(self, ctx, nom, *descr):
+        """Ajoute une catégorie au module."""
+        nom = nom.upper()
+        descr = " ".join(descr)
+        if descr != "":
+            if nom not in self.img["CAT"]:
+                self.img["CAT"][nom] = {"NOM" : nom, "DESC" : descr}
+                fileIO("data/stock/img.json", "save", self.img)
+                await self.bot.say("**Votre catégorie {} à bien été admise**".format(nom))
+            else:
+                await self.bot.say("Cette catégorie semble déjà exister. Utilisez [p]catdel pour supprimer une catégorie.")
         else:
-            await self.bot.say("Sticker déjà chargé.")
+            await self.bot.say("Vous devez ajouter une description rapide à votre catégorie.")
 
-    @sticker.command(pass_context=True, no_pm=True, hidden=True)
+    @imgset.command(pass_context=True, no_pm=True)
     @checks.mod_or_permissions(kick_members=True)
-    async def manualadd(self, ctx, nom, chemin):
-        """Ajoute un sticker à la base de données manuellement."""
-        if nom not in self.base:
-            self.base[nom] = {"CHEMIN" : chemin, "URL" : None}
-            await self.bot.say("Enregistré.")
-            fileIO("data/stick/base.json", "save", self.base)
+    async def catdel(self, ctx, nom):
+        """Retire une catégorie du module."""
+        nom = nom.upper()
+        if nom in self.img["CAT"]:
+            if "AUTRES" not in self.img["CAT"]:
+                self.img["CAT"]["AUTRES"] = {"NOM" : "AUTRES", "DESC" : "Images sans catégories."}
+            for image in self.img["IMG"]:
+                if self.img["IMG"][image]["CAT"] == nom:
+                    self.img["IMG"][image]["CAT"] = "AUTRES"
+            del self.img["CAT"][nom]
+            fileIO("data/stock/img.json", "save", self.img)
+            await self.bot.say("**Votre catégorie {} à été retirée.**\n *Les images ayant cette catégorie sont déplacés dans 'AUTRES'.*".format(nom.title()))
         else:
-            await self.bot.say("Déjà dans la base de données.")
-
-    @commands.command(pass_context=True, no_pm=True, hidden=True)
-    async def convert(self, ctx, base: str, ext: str, url):
-        """Permet de convertir basiquement le format d'un fichier.
-        'base' : Extension fichier au départ
-        'ext' : Extension fichier désiré
-        'url' : Url de téléchargement
-        /!\ Corruption possible. N'utilisez que des liens finissant par une extension. /!\
-        """
-        filename = url.split('/')[-1]
-        channel = ctx.message.channel
-        long = len(base)
-        if base in filename:
-            filename = filename[:-long]
-            filename = filename + ext
-            f = open(filename, 'wb')
-            f.write(request.urlopen(url).read())
-            f.close()
-            file = "data/convert/" + filename
-            os.rename(filename,file)
-            await self.bot.say("Conversion du fichier de {} à {}...".format(base, ext))
-            await asyncio.sleep(1)
-            await self.bot.send_file(channel, file)
-        else:
-            await self.bot.say("Votre extension de base n'est pas reconnue dans l'URL.")
+            await self.bot.say("Cette catégorie n'existe pas. Utilisez [p]catadd pour l'ajouter.")
         
+    @imgset.command(pass_context=True, no_pm=True)
+    @checks.mod_or_permissions(kick_members=True)
+    async def add(self, ctx, nom, cat, url):
+        """Ajoute une image à la base de données.
 
-    @sticker.command(pass_context=True, no_pm=True)
+        Vous pouvez créer des catégories avec [p]img cat"""
+        cat = cat.upper()
+        if cat in self.img["CAT"]:
+            if nom not in self.img["IMG"]:
+                filename = url.split('/')[-1]
+                if ".gif" in filename:
+                    await self.bot.say("*Vérifiez que l'image possède une taille suffisante pour ne pas être cachée par le logo 'GIF'.*")
+                    await asyncio.sleep(0.5)
+                if filename in os.listdir("data/stock/img/"):
+                    exten = filename.split(".")[1]
+                    nomsup = random.randint(1, 9999)
+                    filename = filename.split(".")[0] + str(nomsup) + "." + exten
+                try:
+                    f = open(filename, 'wb')
+                    f.write(request.urlopen(url).read())
+                    f.close()
+                    file = "data/stock/img/" + filename
+                    os.rename(filename,file)
+                    self.img["IMG"][nom] = {"NOM" : nom, "CHEMIN": file, "URL": url, "CAT": cat}
+                    fileIO("data/stock/img.json", "save", self.img)
+                    await self.bot.say("**Fichier {} enregistré localement**".format(filename))
+                except Exception as e:
+                    print("Erreur, impossible de télécharger l'image : {}".format(e))
+                    await self.bot.say("Désolé, mais cette image ne peut pas être téléchargée.")
+            else:
+                await self.bot.say("Image déjà chargée.")
+        else:
+            await self.bot.say("Cette catégorie n'existe pas. Vous pouvez créer des catégories avec [p]img cat.")
+
+    @imgset.command(pass_context=True, no_pm=True)
+    @checks.mod_or_permissions(kick_members=True)
+    async def edit(self, ctx, nom, cat, url : str = None):
+        """Permet d'éditer les données liés à une image. (Catégorie et URL)
+
+        Si aucune URL n'est spécifiée, l'URL originale est conservée."""
+        cat = cat.upper()
+        if cat in self.img["CAT"]:
+            if nom in self.img["IMG"]:
+                if url == None:
+                    url = self.img["IMG"][nom]["URL"]
+                    await self.bot.say("URL conservée...")
+                file = self.img["IMG"][nom]["CHEMIN"]
+                self.img["IMG"][nom] = {}
+                self.img["IMG"][nom] = {"NOM": nom, "CHEMIN": file, "URL": url, "CAT": cat}
+                fileIO("data/stock/img.json", "save", self.img)
+                await self.bot.say("**Données de {} édités**".format(nom))
+            else:
+                await self.bot.say("Ce nom n'est pas dans ma base de données")
+        else:
+            await self.bot.say("Cette catégorie n'existe pas.")
+
+    @imgset.command(pass_context=True, no_pm=True)
     @checks.mod_or_permissions(kick_members=True)
     async def delete(self, ctx, nom):
-        """Supprime un sticker de la base de données."""
-        if nom in self.base:
-            chemin = self.base[nom]["CHEMIN"]
-            os.remove(chemin)
-            del self.base[nom]
-            await self.bot.say("Sticker supprime.")
+        """Supprime une image de la base de données."""
+        if nom in self.img["IMG"]:
+            chemin = self.img["IMG"][nom]["CHEMIN"]
+            file = self.img["IMG"][nom]["CHEMIN"].split('/')[-1]
+            splitted = "/".join(chemin.split('/')[:-1]) + "/"
+            if file in os.listdir(splitted):
+                os.remove(chemin)
+                await self.bot.say("J'ai supprimé le fichier lié.")
+            del self.img["IMG"][nom]
+            await asyncio.sleep(0.5)
+            await self.bot.say("Données supprimées.")
+            fileIO("data/stock/img.json", "save", self.img)
         else:
-            await self.bot.say("Ce sticker n'existe pas.")
+            await self.bot.say("Cette image n'existe pas.")
 
-    @sticker.command(pass_context=True, no_pm=True, hidden=True)
-    @checks.mod_or_permissions(kick_members=True)
-    async def reupload(self, ctx):
-        """Essaye de reupload l'ensemble des images présentes dans base.json et base.bak"""
-        present = os.listdir("data/autre/")
+    @imgset.command(pass_context=True)
+    async def list(self, ctx, cat: str = None):
+        """Liste les images présentes dans la base de données.
+
+        La liste peut être triée par catégorie si spécifiée."""
         msg = ""
-        await self.bot.say("Je vais essayer de faire ça... (Cette opération peut prendre du temps)")
-        for sticker in self.base:
-            if sticker not in present:
-                url = self.base[sticker]["URL"]
-                filename = url.split('/')[-1]
-                if url != None:
-                    try:
-                        f = open(filename, 'wb')
-                        f.write(request.urlopen(url).read())
-                        f.close()
-                        await asyncio.sleep(0,25)
-                        file = "data/autre/" + filename
-                        os.rename(filename,file)
-                        msg += "Fichier {} téléchargé et enregistré.".format(filename)
-                        self.base[nom] = {"CHEMIN": file, "URL": url}
-                        fileIO("data/stick/base.json", "save", self.base)
-                    except Exception as e:
-                        print("Erreur, impossible de télécharger l'image : {}".format(e))
-                        msg += "Désolé, mais {} ne peut pas être téléchargée".format(filename)
-                else:
-                    "Désolé, mais {} ne peut pas être téléchargée car elle ne possède pas d'URL"
+        if cat is None:
+            msg += "__**Images enregistrées par catégories:**__\n"
+            for categorie in self.img["CAT"]:
+                msg += "\n" + "**{}**\n".format(self.img["CAT"][categorie]["NOM"])
+                msg += "__Description:__ *{}*\n".format(self.img["CAT"][categorie]["DESC"])
+                for image in self.img["IMG"]:
+                    if self.img["IMG"][image]["CAT"] == categorie:
+                        msg += "- {}\n".format(self.img["IMG"][image]["NOM"])
+                    else:
+                        pass
             else:
-                pass
+                await self.bot.whisper(msg)
         else:
-            await self.bot.say(msg)
-            await self.bot.say("L'ensemble des fichiers non présents ont été upload.")
-
-    @sticker.command(name = "list", pass_context=True, no_pm=True)
-    async def _list(self, ctx):
-        """Liste les stickers disponibles."""
-        msg = "```Stickers disponibles:\n"
-        for sticker in self.base:
-            msg += ":{}:\n".format(sticker)
-        else:
-            msg += "```"
-        await self.bot.whisper(msg)
+            cat = cat.upper()
+            msg += "__**Images ayant pour catégorie {}:**__\n".format(cat)
+            msg += "__Description:__ *{}*\n".format(self.img["CAT"][cat]["DESC"])
+            for image in self.img["IMG"]:
+                if self.img["IMG"][image]["CAT"] == cat:
+                    msg += "- {}\n".format(self.img["IMG"][image]["NOM"])
+                else:
+                    pass
+            else:
+                await self.bot.whisper(msg)
 
     async def check_msg(self, message):
         channel = message.channel
         if ":" in message.content:
             output = re.compile(':(.*?):', re.DOTALL |  re.IGNORECASE).findall(message.content)
             if output:
-                for sticker in output:
-                    if sticker in self.base:
-                        chemin = self.base[sticker]["CHEMIN"]
-                        url = self.base[sticker]["URL"]
+                for img in output:
+                    if img in self.img["IMG"]:
+                        chemin = self.img["IMG"][img]["CHEMIN"]
+                        url = self.img["IMG"][img]["URL"]
                         try:
                             await self.bot.send_file(channel, chemin)
                         except Exception as e:
                             print("Erreur, impossible d'upload l'image : {}".format(e))
+                            print("Je vais envoyer l'URL lié à la place.")
                             if url != None:
                                 await self.bot.send_message(channel, url)
                             else:
-                                pass
+                                print("Je n'ai pas le lien non plus...")
                     else:
                         pass
             else:
                 pass
         else:
             pass
+            
 
 def check_folders():
-    # create data/sadface if not there
-    if not os.path.exists("data/stick"):
-        print("Creating data/stick folder...")
-        os.makedirs("data/stick")
+    if not os.path.exists("data/stock"):
+        print("Creation du fichier Stock...")
+        os.makedirs("data/stock")
+
+    if not os.path.exists("data/stock/img"):
+        print("Creation du fichier de Stockage d'images...")
+        os.makedirs("data/stock/img")
 
 def check_files():
-    # create server.json if not there
-    # put in default values
     
-    if not os.path.isfile("data/stick/base.json"):
-        print("Creating default stick base.json...")
-        fileIO("data/stick/base.json", "save", {})
+    if not os.path.isfile("data/stock/img.json"):
+        print("Creation du fichier de stock img.json...")
+        fileIO("data/stock/img.json", "save", default_img)
+
+    if not os.path.isfile("data/stock/trg.json"):
+        print("Creation du fichier de stock trg.json...")
+        fileIO("data/stock/trg.json", "save", {})
 
 def setup(bot):
     check_folders()
     check_files()
-    n = Stick(bot)
-    # add an on_message listener
+    n = Stock(bot)
     bot.add_listener(n.check_msg, "on_message")
     bot.add_cog(n)
